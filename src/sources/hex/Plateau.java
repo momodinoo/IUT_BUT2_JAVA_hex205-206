dee453c7d0495dca8560feda12a9dc1c499e8bee
Morgane <morgane4478@gmail.com>
2022-11-15 14:17:46 +0100

    update files

---
+++ src/sources/hex/Plateau.java
@@ -0,0 +1,57 @@
+package sources.hex;
+
+public class Plateau {
+	private final static int CROIX = -1;
+	private final static int ROND = 1;
+	private final static int VIDE = 0;
+	private final static int TAILLE_MAX = 26;
+	
+	private int[][] t;
+
+	public Plateau(int taille) {
+		assert taille > 0 && taille <= TAILLE_MAX;
+		
+		t = new int [taille][taille];
+	}
+	
+	public int taille() {
+		return t.length;
+	}
+	
+	private char symbole (int val) {
+		if (val == CROIX) 
+			return 'X';
+		else if (val == ROND)
+			return 'O';
+		else if (val == VIDE)
+			return '.';
+		else throw new IllegalArgumentException(
+				"" + val + " : symbole inconnu");
+	}
+	
+	private String espaces(int n) {
+		String s = "";
+		for(int i = 0; i < n; ++i)
+			s+= " ";
+		return s;
+	}
+	@Override
+	public String toString() {
+		String s = "";
+		for (int i = 0; i < taille(); ++i)
+			s+=" "+(char)( 'A' + i);
+		s+='\n';
+		for (int lig = 0; lig < taille(); ++lig) {
+			s+= lig+1 + espaces (lig);
+			for (int col = 0; col < taille(); ++col)
+				s+= " "+ symbole(t[col][lig]);
+			s+='\n';
+		}
+		return s;
+	}
+
+
+
+
+
+}
--- src/tests/hex/PlateauTest.java
+++ src/tests/hex/PlateauTest.java
@@ -4,22 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import hex.Plateau;
+import sources.hex.Plateau;
 
 class PlateauTest {
 
 	@Test
 	void test() {
-		final int largeur = 4;
-		final int hauteur = 4;
-		Plateau p = new Plateau(largeur,hauteur);
+		final int taille = 4;
+		Plateau p = new Plateau(taille);
+		assertEquals(taille, p.taille());
 		
+		System.out.println(p);
 		assertEquals(
 				" A B C D\n" + 
 				"1 . . . .\n" + 
 				"2  . . . .\n" + 
 				"3   . . . .\n" + 
 				"4    . . . .\n", p.toString());
-	}
+		}
 
 }
